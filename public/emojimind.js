4ecea58bff7e93410441789228e2d5dbd5c99540
Oskar Rough <oskar@rough.dk>
2016-09-23 21:10:05 +0200

    Add more tests, lint and a bit of refactor

--- public/emojimind.js
+++ public/emojimind.js
@@ -1,22 +1,22 @@
-const em = {};
+const em = {}
 
 em.createCode = (symbols, length) => {
-	let newCode = [];
+	let newCode = []
 	for (let i = 0; i < length; i++) {
-		const random = em.numberBetween(0, symbols.length);
-		newCode.push(symbols[random]);
+		const random = em.numberBetween(0, symbols.length)
+		newCode.push(symbols[random])
 	}
-	return newCode;
-};
+	return newCode
+}
 
 // Returns a random integer between min (included) and max (excluded)
-em.numberBetween = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+em.numberBetween = (min, max) => Math.floor(Math.random() * (max - min)) + min
 
 // Copies an array
-em.copyArray = array => array.map(arr => arr.slice());
+em.copyArray = array => array.map(arr => arr.slice())
 
 // Checks whether an object is empty or not
-em.isEmptyObject = obj => Object.keys(obj).length === 0 && obj.constructor === Object;
+em.isEmptyObject = obj => Object.keys(obj).length === 0 && obj.constructor === Object
 
 // Takes two arrays and returns an object with counts of black and white pins.
 // Example: getHints(['1', '2', '3', '4'], ['2', '1', '1', '4']) => {blacks: 1, whites: 1}
@@ -24,41 +24,40 @@ em.isEmptyObject = obj => Object.keys(obj).length === 0 && obj.constructor === O
 // A white is given if it is the right symbol but in wrong position.
 
 em.getHints = (answer, guess, debug) => {
-	var hints = {blacks: 0, whites: 0};
-	var skiplist = [];
-	var skiplist2 = [];
+	var hints = {blacks: 0, whites: 0}
+	var skiplist = []
+	var skiplist2 = []
 
 	for (let slot in answer) {
 		if (answer[slot] === guess[slot]) {
-			hints.blacks++;
-			skiplist.push(slot);
+			hints.blacks++
+			skiplist.push(slot)
 		}
 	}
 
-	if (debug) console.log(`skipping slots: [${skiplist}]`);
+	if (debug) console.log(`skipping slots: [${skiplist}]`)
 
 	for (let g in guess) {
-		if (debug) console.log(`is ${guess[g]} from ${guess} a white in ${answer}?`);
+		if (debug) console.log(`is ${guess[g]} from ${guess} a white in ${answer}?`)
 		if (skiplist.includes(g)) {
-			if (debug) console.log(`skiplist ${guess[g]} ${answer[g]}`);
-			continue;
+			if (debug) console.log(`skiplist ${guess[g]} ${answer[g]}`)
+			continue
 		}
 		for (let a in answer) {
 			if (skiplist.includes(a) || skiplist2.includes(a)) {
-				if (debug) console.log('- skiplist');
-				continue;
+				if (debug) console.log('- skiplist')
+				continue
 			}
 			if (guess[g] === answer[a]) {
-				if (debug) console.log(`- found a white`);
-				skiplist2.push(a);
-				hints.whites++;
-				break;
-			} else {
-				if (debug) console.log(`- skip: no match`);
+				if (debug) console.log(`- found a white`)
+				skiplist2.push(a)
+				hints.whites++
+				break
 			}
+			if (debug) console.log(`- skip: no match`)
 		}
 	}
-	return hints;
-};
+	return hints
+}
 
-module.exports = em;
+module.exports = em
--- public/index.js
+++ public/index.js
@@ -1,13 +1,13 @@
-/* global em, Vue */
+/* global window, em, Vue */
 
 // Returns a boolean if the object is emtpy
-Vue.filter('isEmptyObject', value => em.isEmptyObject(value));
+Vue.filter('isEmptyObject', value => em.isEmptyObject(value))
 
 // Because our 'guess' array contains null values we need to filter them out before we can check the length.
 Vue.filter('invalidGuess', (array, max) => {
-  console.log(array, max);
-  return array.filter(v => v).length !== max;
-});
+	console.log(array, max)
+	return array.filter(v => v).length !== max
+})
 
 const SelectGuess = Vue.component('select-guess', {
 	props: ['guess', 'symbols', 'disabled'],
@@ -20,7 +20,7 @@ const SelectGuess = Vue.component('select-guess', {
 			</select>
 		</li>
 	</ul>`
-});
+})
 
 const vm = new Vue({
 	el: '#emojimind',
@@ -34,61 +34,50 @@ const vm = new Vue({
 	},
 	computed: {
 		buttonLabel() {
-			return this.code.length ? 'Give me a new code' : 'Start a new game';
+			return this.code.length ? 'Give me a new code' : 'Start a new game'
 		}
 	},
 	methods: {
 		newGame() {
-			this.code = em.createCode(this.symbols, this.codeLength);
-			this.guesses = this.createGuesses();
-			// this.tryTest();
+			this.code = em.createCode(this.symbols, this.codeLength)
+			this.guesses = this.createGuesses()
 		},
-		tryTest() {
-			this.code = ['6', '3', '2', '2'];
-			this.guesses[0].guess = ['6', '2', '3', '2'];
-			this.guesses[1].guess = ['3', '3', '6', '3'];
-			this.guesses[2].guess = ['3', '3', '3', '6'];
-			this.guesses[3].guess = ['3', '2', '3', '6'];
-			this.guesses[4].guess = ['2', '1', '3', '6'];
-			this.guesses[5].guess = ['3', '3', '3', '3'];
-			this.guesses.map((guess, index) => this.tryGuess(index));
-		},
-		// Returns a data structure: [{guess: [null, null, null, null], pins: []}, ...]
 		createGuesses() {
-			let guesses = [];
+			let guesses = []
 			for (let i = 0; i < this.maxGuesses; i++) {
 				let row = {
 					guess: [],
 					pins: {}
-				};
-				for (let k = 0; k < this.codeLength; k++) {
-					row.guess.push(null);
 				}
-				guesses.push(row);
+				for (let k = 0; k < this.codeLength; k++) {
+					// We fill the array with null values to give it a length
+					row.guess.push(null)
+				}
+				guesses.push(row)
 			}
-			return guesses;
+			return guesses
 		},
 		tryGuess(index, event) {
-			event.preventDefault();
-			const guess = this.guesses[index].guess;
-			const hasTheRightLength = guess.filter(g => g).length === this.codeLength;
+			event.preventDefault()
+			const guess = this.guesses[index].guess
+			const hasTheRightLength = guess.filter(g => g).length === this.codeLength
 			if (!hasTheRightLength) {
-				throw new Error(`Your guess isn't complete. Does it have ${this.codeLength} symbols?`);
+				throw new Error(`Your guess isn't complete. Does it have ${this.codeLength} symbols?`)
 			}
-			const isLastGuess = index === this.guesses.length - 1;
-			const pins = em.getHints(this.code, guess);
-			console.log(pins);
-			Vue.set(this.guesses[index], 'pins', pins);
-			const win = pins.blacks === this.codeLength;
-			if (win) {
-				alert('Correct.');
-				console.log('Correct.');
-			} else {
-				console.log('Wrong.');
-				if (isLastGuess) {
-					alert(`Mwa ha ha, you lose. I am the mastermind. Not you.`);
-				}
+			const isLastGuess = index === this.guesses.length - 1
+			const pins = em.getHints(this.code, guess)
+			Vue.set(this.guesses[index], 'pins', pins)
+			this.checkIfWeWon(pins.blacks === this.codeLength)
+		},
+		checkIfWeWon(winCondition) {
+			if (winCondition) {
+				console.log('Correct.')
+				return window.alert('Correct.')
+			}
+			console.log('Wrong.')
+			if (isLastGuess) {
+				window.alert(`Mwa ha ha, you lose. I am the mastermind. Not you.`)
 			}
 		}
 	}
-});
+})
